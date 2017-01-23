1920109e7e4766af09fe82cbd6fccd1d88d3a207
Jacob Ingalls <jacob.ingalls@thirdship.com>
2017-01-22 20:47:42 -0600

    Use more descriptive variable names

--- db/sqlite.go
+++ db/sqlite.go
@@ -9,21 +9,23 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// NewSQLiteVolumeDatabase creates a new SQLVolumeDatabase, saving the database at dbPath.
-func NewSQLiteVolumeDatabase(dbPath string) SQLVolumeDatabase {
-
-	var queries VolumeDatabaseQueries
-	queries.volumesCreateTableSQL = `CREATE TABLE IF NOT EXISTS volumes (
+// SQLiteSQLOverrides defines a list of qurries that need to differ from the defaults in order for sqlite to function correctly.
+var SQLiteSQLOverrides = VolumeDatabaseQueries{
+	volumesCreateTableSQL: `CREATE TABLE IF NOT EXISTS volumes (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE,
         mountpoint TEXT
-    )`
+    )`,
 
-	queries.mountsCreateTableSQL = `CREATE TABLE IF NOT EXISTS mounts (
+	mountsCreateTableSQL: `CREATE TABLE IF NOT EXISTS mounts (
         volume_id INTEGER NOT NULL,
         requester_id TEXT NOT NULL,
         count INTEGER NOT NULL
-    )`
+    )`,
+}
+
+// NewSQLiteVolumeDatabase creates a new SQLVolumeDatabase, saving the database at dbPath.
+func NewSQLiteVolumeDatabase(dbPath string) SQLVolumeDatabase {
 
 	// If the database path was not set, then we are resposible for managing it.
 	if dbPath == "" {
@@ -42,5 +44,5 @@ func NewSQLiteVolumeDatabase(dbPath string) SQLVolumeDatabase {
 	}
 
 	// Create the connection
-	return NewSQLVolumeDatabase("sqlite3", path.Join(dbPath, "db"), VolumeDatabaseQueries{})
+	return NewSQLVolumeDatabase("sqlite3", path.Join(dbPath, "db"), SQLiteSQLOverrides)
 }
