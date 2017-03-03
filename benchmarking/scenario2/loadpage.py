d93bf43eb4100cb3161ce4c7e0d3ed2c71ebf94d
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2017-03-03 17:45:18 -0600

    can now take load generator results and put in csv format. for some reason whenever i ran with -o option it would hang. so i just wrote it myself. can now begin to collect metrics.

--- benchmarking/scenario2/loadpage.py
+++ benchmarking/scenario2/loadpage.py
@@ -1,13 +1,82 @@
+# loads page using http load generator from https://github.com/rakyll/hey
+# also outputs in csv format the return results from the generator
+
+# layout of csv file is:
+# summary
+# first five are field, time (in secs)
+# last two are field, amt (in bytes)
+
+# response time historgram
+# time (in secs), number of occurences
+
+# latency distribution
+# percent in this time, time (in secs)
+
 import os
 import logging
 import time
 import sys
 import subprocess
+import re
 
 def main():
-    results = subprocess.call(["hey", "-m=GET", "-disable-compression", "disable-keepalive", "https://google.com"])
+    # subprocess.call(["hey", "-m=GET", "-disable-compression", "https://google.com"])
+    # ret = subprocess.check_output(["hey", "-m=GET", "-disable-compression", "http://localhost:8000/?p=20"])
+    # hey -m=GET -disable-compression https://google.com
+    ret = subprocess.check_output(["hey", "-m=GET", "-disable-compression", "https://google.com"])
 
-    results = subprocess.call(["hey", "-m=GET", "-disable-compression", "disable-keepalive", "http://localhost:8000/?p=20"])
+    # ret = "hello\n\nworld"
+    # print ret
+
+    ret = re.sub(r'(\n\n)', "\n", ret)
+    res = []
+
+    ret = ret.splitlines()
+    it = ret.__iter__()
+    while True:
+        try:
+            line = it.next()
+            while(line.lower() != "All requests done.".lower()): line = it.next()
+
+            # summary results
+            line = it.next()
+            # res.append(line.lower())
+
+            line = it.next()
+            while(line.lower() != "Status code distribution:".lower()):
+
+                tmp = re.sub(r'(\s)+', "", line)
+                tmp = re.sub(r'(secs)', "", tmp)
+                tmp = re.sub(r'(bytes)', "", tmp)
+                tmp = re.sub(r':', ",", tmp).lower()
+                res.append(tmp)
+                line = it.next()
+
+            while(line.lower() != "Response time histogram:".lower()): line = it.next()
+
+            # response time histogram
+            line = it.next()
+            while(line.lower() != "Latency distribution:".lower()):
+                tmp = re.sub(r'^[\s\W]+', "", line)
+                tmp = re.sub(r'[\s\W]+$', "", tmp)
+                tmp = re.sub(r'(\s\[)+', ",", tmp).lower()
+                res.append(tmp)
+                line = it.next()
+
+            # latency distribution
+            line = it.next()
+            while(line):
+                tmp = re.sub(r'^[\s\W]+', "", line)
+                tmp = re.sub(r'(\D)+$', "", tmp)
+                tmp = re.sub(r'(%\sin\s)', ",", tmp).lower()
+                res.append(tmp)
+                line = it.next()
+        except StopIteration:
+            break
+
+    out = open("out", "w")
+    out.write('\n'.join(res))
+    out.close()
 
 if __name__ == '__main__':
     hostname = os.uname()[1]
