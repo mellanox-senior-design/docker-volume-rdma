a08759630e88c84b8213b13b66b06188c59af437
Cesar Gonzalez <cesar_gonzalez@utexas.edu>
2017-03-03 18:19:21 -0600

    runs with in-memory, needs to be linked to mysql container, possible cross-compatibility issues with sqlite3

--- integration-testing/run.sh
+++ integration-testing/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
-# Exit immediately if a pipeline [...] returns a non-zero status.
+# -e : Exit immediately if a pipeline [...] returns a non-zero status.
+
+if [ -z "$GOPATH" ]; then
+    echo 'GOPATH is not set' >&2
+    exit 2
+fi
 
 # change to docker-volume-rdma directory
 cd ..
@@ -7,13 +12,12 @@ cd ..
 # download packages named by import path along with dependencies
 # then install named packages like 'go install'
 # '...' matches all subdirectories
-go get -t -d -v ./...
+go get -t -v ./...
 
-# build project
-go build -v
+GOOS=linux go build -v
 
 # move executable binary to Dockerfile's context
-mv docker-volume-rdma ./integration-testing/DockerVolumeRDMA
+mv docker-volume-rdma ./integration-testing
 
 # change directory to integration-testing
 cd integration-testing
@@ -21,8 +25,11 @@ cd integration-testing
 # build Docker image named docker-volume-rdma-integration-test
 docker build -t docker-volume-rdma-integration-test-image .
 
+# remove executable binary
+rm docker-volume-rdma
+
 # start container with port 8080 exposed
 docker run --rm -p 8080:8080 --name docker-volume-rdma-integration-test docker-volume-rdma-integration-test-image
 
 # run tests that are tagged with integration
-# go test -v -tags integration ./...
+# go test -v -tags integration ../...
