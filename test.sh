fd29f29ae9e0a54fb7cac97ce1d72ff28463fa84
John Starich <john.starich@thirdship.com>
2017-03-06 15:31:36 -0600

    Add labels and failure messages

--- test.sh
+++ test.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+function red() {
+	echo $'\033[1;31m'"$@"$'\033[m'
+}
+
+function green() {
+	echo $'\033[1;32m'"$@"$'\033[m'
+}
+
 GIT_BRANCH=$(
 	git rev-parse --abbrev-ref HEAD |
 	tr '[:upper:]' '[:lower:]' |
 	sed 's#[^a-z0-9._-]#-#'
 )
 
-docker build --tag docker-volume-rdma:"$GIT_BRANCH" .
-docker rmi docker-volume-rdma:"$GIT_BRANCH"
-./benchmarking/scenarios/test.sh
+set -e
 
+green Unit Tests
+if ! docker build --tag docker-volume-rdma:"$GIT_BRANCH" .; then
+	red 'Failed to run unit tests'
+fi
+docker rmi docker-volume-rdma:"$GIT_BRANCH"
+
+green Benchmarks
+if ! ./benchmarking/scenarios/test.sh; then
+	red 'Failed to run benchmarks'
+fi
