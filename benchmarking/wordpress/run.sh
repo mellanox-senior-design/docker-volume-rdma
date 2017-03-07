e9fe550d416cba01b4ab8076980b9b403ed9ff43
Jacob Ingalls <jacob.ingalls@thirdship.com>
2017-03-07 16:27:17 -0600

    Automatically configure database

--- benchmarking/wordpress/run.sh
+++ benchmarking/wordpress/run.sh
@@ -9,6 +9,9 @@ done
 
 echo "MySql is up!"
 
+sleep 5
+flyway-4.1.1/flyway -user=root -password=wordpress -url=jdbc:mysql://mysql -schemas=wordpress migrate
+
 while ! nc -z wordpress 80; do
   echo "Waiting for WordPress to launch (80)."
   sleep 1
@@ -16,5 +19,4 @@ done
 
 echo "WordPress blog is up!"
 
-sleep 5
 python bench.py
