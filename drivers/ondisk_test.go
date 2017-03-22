d41520f88dab59702489b95d03561af7e720425f
jjalfaro9 <jalfaro@utexas.edu>
2017-03-22 14:19:46 -0500

    update per John's comments

--- drivers/ondisk_test.go
+++ drivers/ondisk_test.go
@@ -1,16 +1,12 @@
 package drivers
 
-import (
-	"os"
-	"path"
-	"testing"
-)
+import "testing"
 
 func TestNewOnDiskStorageController(t *testing.T) {
 	t.Parallel()
 	sc := NewOnDiskStorageController("test/controlla")
 	if sc.FSPath != "test/controlla" {
-		t.Fatal("Storage controller created in wrond dir ", sc.FSPath)
+		t.Fatal("Storage controller created in wrong dir ", sc.FSPath)
 	}
 }
 
@@ -52,33 +48,37 @@ func TestSCMount(t *testing.T) {
 	mountedPath, err := sc.Mount("formulavol1")
 
 	if err != nil {
+		t.Error(err)
+	}
+
+	if mountedPath != "test/controlla/formulavol1" {
+		t.Error("Did not receive expected path, instead got ", mountedPath)
+	}
+
+	mountedPath, err = sc.Mount("formulavol1")
+
+	if err != nil {
+		t.Error(err, " strange, should have been able to mount the same vol")
+	}
+
+	if mountedPath != "test/controlla/formulavol1" {
+		t.Error("If I am mounting the same volume, the same path should be kept ", mountedPath)
+	}
+
+	err = sc.Unmount("formulavol1")
+
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	if mountedPath != "test/controlla/formulavol1" {
-		t.Fatal("Did not receive expected path, instead got ", mountedPath)
-	}
-
 	mountedPath, err = sc.Mount("formulavol1")
 
 	if err != nil {
-		t.Fatal(err, " strange, should have been able to mount the same vol")
+		t.Error(err, " we should not fail though.")
 	}
 
 	if mountedPath != "test/controlla/formulavol1" {
-		t.Fatal("If I am mounting the same volume, the same path should be kept ", mountedPath)
-	}
-
-	os.Rename(mountedPath, path.Join(path.Dir(mountedPath), path.Base(mountedPath)+".unmounted"))
-
-	mountedPath, err = sc.Mount("formulavol1")
-
-	if err != nil {
-		t.Fatal(err, " we should not fail though.")
-	}
-
-	if mountedPath != "test/controlla/formulavol1" {
-		t.Fatal("Did not expect ", mountedPath, " to be the mountedPath")
+		t.Error("Did not expect ", mountedPath, " to be the mountedPath")
 	}
 
 }
@@ -90,7 +90,7 @@ func TestSCUnmount(t *testing.T) {
 	err := sc.Unmount("formulavol2")
 
 	if err == nil {
-		t.Fatal("There should be an error when unmounting a volume that has not been mounted")
+		t.Error("There should be an error when unmounting a volume that has not been mounted")
 	}
 
 	_, err = sc.Mount("formulavol2")
@@ -108,7 +108,7 @@ func TestSCUnmount(t *testing.T) {
 	err = sc.Unmount("formulavol2")
 
 	if err == nil {
-		t.Fatal("Should have received an error for unmounting a volume twice")
+		t.Error("Should have received an error for unmounting a volume twice")
 	}
 
 }
@@ -126,7 +126,7 @@ func TestSCDelete(t *testing.T) {
 	err = sc.Delete("volume3")
 
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	_, err = sc.Mount("volume4")
@@ -134,16 +134,19 @@ func TestSCDelete(t *testing.T) {
 		t.Fatal(err)
 	}
 	err = sc.Unmount("volume4")
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	err = sc.Delete("volume4")
 
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	err = sc.Delete("vol5")
 
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 }
