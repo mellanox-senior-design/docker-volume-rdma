fa3a30755be967ef76841a9f9e5efd373ee7964d
Cesar Gonzalez <cesar_gonzalez@utexas.edu>
2017-04-23 21:17:43 -0500

    add tests for List and Path

--- main_integration_test.go
+++ main_integration_test.go
@@ -59,6 +59,7 @@ func TestCapabilities(t *testing.T) {
 }
 
 func TestList(t *testing.T) {
+	/**************************LIST********************************/
 	// Get the list of volumes registered with the plugin.
 	t.Logf("POST /VolumeDriver.List")
 
@@ -90,7 +91,6 @@ func TestList(t *testing.T) {
 	if resp.Body == nil {
 		t.Fatal("resp.Body is nil!")
 	}
-	defer resp.Body.Close()
 
 	var r volume.Response
 
@@ -105,6 +105,656 @@ func TestList(t *testing.T) {
 
 	if len(r.Volumes) != 0 {
 		t.Fatal("List of volumes should be 0! Actual:", len(r.Volumes))
+	}
+
+	resp.Body.Close()
+
+	/**************************CREATE********************************/
+	// Get a Docker approved random name
+	volumeName0 := namesgenerator.GetRandomName(0)
+
+	// Create a new volume with name and options.
+	t.Logf("POST /VolumeDriver.Create")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s, Options: map[string]string{}})", volumeName0)
+	jsn, err = json.Marshal(volume.Request{
+		Name:    volumeName0,
+		Options: map[string]string{},
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Create", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to create volume! {Name: ", volumeName0, "} ", r.Err)
+	}
+
+	resp.Body.Close()
+
+	/**************************LIST********************************/
+	// Get the list of volumes registered with the plugin.
+	t.Logf("POST /VolumeDriver.List")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{})")
+	jsn, err = json.Marshal(volume.Request{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.List", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to list volumes!", r.Err)
+	}
+
+	if len(r.Volumes) != 1 {
+		t.Fatal("List of volumes should be 1! Actual:", len(r.Volumes))
+	}
+
+	resp.Body.Close()
+
+	/**************************CREATE********************************/
+	// Get a Docker approved random name
+	volumeName1 := namesgenerator.GetRandomName(1)
+
+	// Create a new volume with name and options.
+	t.Logf("POST /VolumeDriver.Create")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s, Options: map[string]string{}})", volumeName1)
+	jsn, err = json.Marshal(volume.Request{
+		Name:    volumeName1,
+		Options: map[string]string{},
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Create", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to create volume! {Name: ", volumeName1, "} ", r.Err)
+	}
+
+	resp.Body.Close()
+
+	/**************************LIST********************************/
+	// Get the list of volumes registered with the plugin.
+	t.Logf("POST /VolumeDriver.List")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{})")
+	jsn, err = json.Marshal(volume.Request{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.List", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to list volumes!", r.Err)
+	}
+
+	if len(r.Volumes) != 2 {
+		t.Fatal("List of volumes should be 2! Actual:", len(r.Volumes))
+	}
+
+	for i := 0; i < len(r.Volumes); i++ {
+		name := r.Volumes[i].Name
+		if name != volumeName0 && name != volumeName1 {
+			t.Fatal("List returned the wrong volume. Expected:", volumeName0, "or", volumeName1, "but received "+r.Volumes[i].Name)
+		}
+	}
+
+	resp.Body.Close()
+
+	/**************************REMOVE********************************/
+	// Remove (Delete) a paricular volume.
+	t.Logf("POST /VolumeDriver.Remove")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName0)
+	jsn, err = json.Marshal(volume.Request{
+		Name: volumeName0,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Remove", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to delete volume! {Name: ", volumeName0, "} ", r.Err)
+	}
+
+	resp.Body.Close()
+
+	/**************************REMOVE********************************/
+	// Remove (Delete) a paricular volume.
+	t.Logf("POST /VolumeDriver.Remove")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName1)
+	jsn, err = json.Marshal(volume.Request{
+		Name: volumeName1,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Remove", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to delete volume! {Name: ", volumeName1, "} ", r.Err)
+	}
+}
+
+func TestPath(t *testing.T) {
+	// Get a Docker approved random name
+	volumeName := namesgenerator.GetRandomName(0)
+
+	/**************************PATH********************************/
+	// Path reminds docker of the path a particular volume is attached to.
+	t.Logf("POST /VolumeDriver.Path")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName)
+	jsn, err := json.Marshal(volume.Request{
+		Name: volumeName,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body := bytes.NewBuffer(jsn)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Path", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	var r volume.Response
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err == "" {
+		t.Fatal("Path of a volume that hasn't been created should generate an error")
+	}
+
+	resp.Body.Close()
+
+	/**************************CREATE********************************/
+	// Create a new volume with name and options.
+	t.Logf("POST /VolumeDriver.Create")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s, Options: map[string]string{}})", volumeName)
+	jsn, err = json.Marshal(volume.Request{
+		Name:    volumeName,
+		Options: map[string]string{},
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Create", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to create volume! {Name: ", volumeName, "} ", r.Err)
+	}
+
+	resp.Body.Close()
+
+	/**************************PATH********************************/
+	// Path reminds docker of the path a particular volume is attached to.
+	t.Logf("POST /VolumeDriver.Path")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName)
+	jsn, err = json.Marshal(volume.Request{
+		Name: volumeName,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Path", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Error occured while getting path of", volumeName, r.Err)
+	}
+
+	if r.Mountpoint != "" {
+		t.Fatal("Mountpoint should be: \"\" since volume hasn't been mounted")
+	}
+
+	resp.Body.Close()
+
+	/**************************MOUNT********************************/
+	// Mount a paricular volume to the host.
+	t.Logf("POST /VolumeDriver.Mount")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.MountRequest{Name: %s, ID: \"42\"})", volumeName)
+	jsn, err = json.Marshal(volume.MountRequest{
+		Name: volumeName,
+		ID:   "42",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Mount", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Error occured while mounting:", volumeName, r.Err)
+	}
+
+	if r.Mountpoint != "/etc/docker/mounts/"+volumeName {
+		t.Fatal("Expected Mountpoint: /etc/docker/mounts/" + volumeName + " Actual: " + r.Mountpoint)
+	}
+
+	resp.Body.Close()
+
+	/**************************PATH********************************/
+	// Path reminds docker of the path a particular volume is attached to.
+	t.Logf("POST /VolumeDriver.Path")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName)
+	jsn, err = json.Marshal(volume.Request{
+		Name: volumeName,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Path", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Error occured while getting path of", volumeName, r.Err)
+	}
+
+	if r.Mountpoint != "/etc/docker/mounts/"+volumeName {
+		t.Fatal("Expected Mountpoint: /etc/docker/mounts/" + volumeName + " Actual: " + r.Mountpoint)
+	}
+
+	resp.Body.Close()
+
+	/**************************UNMOUNT********************************/
+	// Unmount a paricular volume from host (if no other mount requests active).
+	t.Logf("POST /VolumeDriver.Unmount")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.UnmountRequest{Name: %s, ID: \"42\"})", volumeName)
+	jsn, err = json.Marshal(volume.UnmountRequest{
+		Name: volumeName,
+		ID:   "42",
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Unmount", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Error occured while unmounting:", volumeName, r.Err)
+	}
+
+	resp.Body.Close()
+
+	/**************************REMOVE********************************/
+	// Remove (Delete) a paricular volume.
+	t.Logf("POST /VolumeDriver.Remove")
+
+	// Create json for request - local variable json masks the global symbol json referring to the JSON module
+	t.Logf("json.Marshal(volume.Request{Name: %s})", volumeName)
+	jsn, err = json.Marshal(volume.Request{
+		Name: volumeName,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create request to server
+	body = bytes.NewBuffer(jsn)
+	client = &http.Client{}
+	req, err = http.NewRequest("POST", "http://plugin:8080/VolumeDriver.Remove", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Fetch request
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatal("Failed to connect to server! ", err)
+	}
+
+	if resp == nil {
+		t.Fatal("resp is nil!")
+	}
+	if resp.Body == nil {
+		t.Fatal("resp.Body is nil!")
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if r.Err != "" {
+		t.Fatal("Failed to delete volume! {Name: ", volumeName, "} ", r.Err)
 	}
 }
 
